dca1adedacdaeadb64394d58e18a88e112c33aab
Peter Banka <pbanka@fastly.com>
2019-02-11 14:25:05 -0800

    💥 taking the pre-wiring off the submit button in ui-form

--- app/components/ui-form/component.ts
+++ app/components/ui-form/component.ts
@@ -1,18 +1,14 @@
 import hbs from "htmlbars-inline-precompile";
 import Component from "@ember/component";
-import { tagName } from "@ember-decorators/component";
 
-// TODO: Pull `form` out and see things break.
-@tagName("form")
 export default class UiForm extends Component {
   // Template
   // ---------------------------------------------------------------------------
-  // TODO: take out pre-wiring of submit button
   public layout = hbs`
     {{yield (hash
       label=(component "ui-form/label")
       input=(component "ui-form/input")
-      submit=(component "ui-button" id="submit" onClick=onSubmit type="submit")
+      submit=(component "ui-button" id="submit" type="submit")
     )}}
   `;
   // Passed properties
